f8e59b2a523746151c9cbcd3365e61cafa3de55c
Aadarsh Jr <55702370+aadarshjr123@users.noreply.github.com>
2025-09-22 21:22:01 +0100

    feat(Day-4) - Move Zeroes - O(n)

---
+++ leetCode/283.move-zeroes.js
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode id=283 lang=javascript
+ *
+ * [283] Move Zeroes
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+
+function moveZeroes(nums) {
+  let write = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] !== 0) {
+      let temp = nums[i];
+      nums[i] = nums[write];
+      nums[write] = temp;
+      write++;
+    }
+  }
+  return nums;
+}
+// @lc code=end
