17829c84930e3b2f40e64309eda73bc2b36c91b3
Aadarsh Jr <55702370+aadarshjr123@users.noreply.github.com>
2025-09-22 21:42:05 +0100

    feat(Day-4) - Sort Colors (Medium) - O(n)

---
+++ leetCode/75.sort-colors.js
@@ -0,0 +1,30 @@
+/*
+ * @lc app=leetcode id=75 lang=javascript
+ *
+ * [75] Sort Colors
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+function sortColors(nums) {
+  let low = 0,
+    mid = 0;
+  high = nums.length - 1;
+  while (mid <= high) {
+    if (nums[mid] === 0) {
+      [nums[low], nums[mid]] = [nums[mid], nums[low]];
+      low++;
+      mid++;
+    } else if (nums[mid] === 1) {
+      mid++;
+    } else if (nums[mid] === 2) {
+      [nums[mid], nums[high]] = [nums[high], nums[mid]];
+      high--;
+    }
+  }
+  return nums;
+}
+// @lc code=end
