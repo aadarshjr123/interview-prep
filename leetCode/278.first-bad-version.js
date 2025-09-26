157eb97c8831d26cad54c1f1ea25f75194c725e0
Aadarsh Jr <55702370+aadarshjr123@users.noreply.github.com>
2025-09-26 23:51:51 +0100

    feat(day-8): First Bad Version - (Easy) - O(log n)

---
+++ leetCode/278.first-bad-version.js
@@ -0,0 +1,46 @@
+/*
+ * @lc app=leetcode id=278 lang=javascript
+ *
+ * [278] First Bad Version
+ */
+
+// @lc code=start
+/**
+ * Definition for isBadVersion()
+ *
+ * @param {integer} version number
+ * @return {boolean} whether the version is bad
+ * isBadVersion = function(version) {
+ *     ...
+ * };
+ */
+
+/**
+ * @param {function} isBadVersion()
+ * @return {function}
+ */
+var solution = function (isBadVersion) {
+  /**
+   * @param {integer} n Total versions
+   * @return {integer} The first bad version
+   */
+  return function (n) {
+    let left = 0,
+      right = n;
+    while (left < right) {
+      let mid = left + Math.floor((right - left) / 2);
+      if (isBadVersion(mid)) {
+        right = mid;
+      } else {
+        left = mid + 1;
+      }
+    }
+
+    return left;
+  };
+};
+
+// @lc code=end
+
+// time: O(logn)
+// space: O(1)
