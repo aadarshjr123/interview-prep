aa4086e079d8d6cd82d54df6f695036d223feb8b
Aadarsh Jr <55702370+aadarshjr123@users.noreply.github.com>
2025-09-27 21:54:13 +0100

    feat(day-9): updated the comments slope-check logic

--- leetCode/162.find-peak-element.js
+++ leetCode/162.find-peak-element.js
@@ -35,3 +35,8 @@ var findPeakElement = function (nums) {
 // if mid < mid+1 => left = mid + 1
 // else right = mid - 1
 // return left
+
+//slope-check logic
+// if mid < mid+1 => right slope => move to right
+// else left slope => move to left
+// when left meets right, it's the peak element
