f9694e0fee893faa50a9bc5f8d0d691e27dabbdb
Aadarsh Jr <55702370+aadarshjr123@users.noreply.github.com>
2025-09-27 21:51:33 +0100

    feat(day-9): [162] Find Peak Element - (Medium) - O(logn)

---
+++ leetCode/162.find-peak-element.js
@@ -0,0 +1,37 @@
+/*
+ * @lc app=leetcode id=162 lang=javascript
+ *
+ * [162] Find Peak Element
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findPeakElement = function (nums) {
+  let left = 0,
+    right = nums.length - 1;
+
+  while (left <= right) {
+    let mid = left + Math.floor((right - left) / 2);
+    if (nums[mid] < nums[mid + 1]) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+
+  return left;
+};
+
+// @lc code=end
+
+// time: O(logn)
+// space: O(1)
+// binary search
+// [1,2,3,1] => 2
+// [1,2,1,3,5,6,4] => 5
+// if mid < mid+1 => left = mid + 1
+// else right = mid - 1
+// return left
