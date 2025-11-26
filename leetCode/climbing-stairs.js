e0a1658b16b7efd8ffca814b40669ab1aed7db7a
aadarshjr123 <aadarshsasikumar@example.com>
2025-11-26 02:37:06 +0100

    ✨ feat: add solutions for Climbing Stairs [70] and Subsets [78] - O(n)

---
+++ leetCode/climbing-stairs.js
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode id=70 lang=javascript
+ *
+ * [70] Climbing Stairs
+ */
+
+// @lc code=start
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var climbStairs = function(n) {
+    let memo = {};
+    function dfs(i) {
+        if(i===n) return 1;
+        if(i>n) return 0;
+        if(memo[i] !== undefined) return memo[i]
+        let result = dfs(i+1) + dfs(i+2);
+        memo[i] = result;
+        return result;
+    }
+
+    return dfs(0);
+};
+// @lc code=end
+
+
---
+++ leetCode/mock/subsets.js
@@ -0,0 +1,31 @@
+/*
+ * @lc app=leetcode id=78 lang=javascript
+ *
+ * [78] Subsets
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var subsets = function(nums) {
+  
+    let result = [];
+    function dfs(i,subset) {
+        if(i === nums.length) {
+            result.push([...subset])
+            return
+        }
+
+        dfs(i+1,subset)
+        subset.push(nums[i])
+        dfs(i+1,subset)
+        subset.pop()
+    }
+
+    dfs(0,[])
+    return result
+};
+// @lc code=end
+
